d5043ed7c15d3a3bf1362593f7c01d22f3850f1f
Dieter Luypaert <dieterluypaert@gmail.com>
2016-02-11 16:58:38 +0100

    expand javascript support

--- spec/javascript.js
+++ spec/javascript.js
@@ -1,16 +1,116 @@
-function $initHighlight(block, flags) {
-  try {
-    if (block.className.search(/\bno\-highlight\b/) != -1)
-      return processBlock(block.function, true, 0x0F) + ' class=""';
-  } catch (e) {
-    /* handle exception */
-    var e4x =
-        <div>Example
-            <p>1234</p></div>;
-  }
-  for (var i = 0 / 2; i < classes.length; i++) { // "0 / 2" should not be parsed as regexp
-    if (checkCondition(classes[i]) === undefined)
-      return /\d+[\s/]/g;
-  }
-  console.log(Array.every(classes, Boolean));
+// jsdoc
+
+/**
+ * @param {string[]} str
+ * @return {string}
+ */
+
+// import and export
+
+import { assign } from 'lodash';
+
+export const TEST = 5;
+
+export default {};
+
+// variables and values
+
+var abc, def;
+var n = 0 + n++;
+var s = 'string';
+var b = true || false;
+var { n } = { n: null };
+var u = undefined;
+let l = 'scoped'.length;
+let oct = 0x0F;
+const [ LETTERS ] = 'ok' && my.nested['alphabet'].property[0];
+const r = /^([A-Z\.].)a+b*c?$/igmyu;
+const intr = `Hello ${world.toUpperCase()}!`;
+const arr = [];
+
+// functions
+
+calling();
+new Calling();
+calling.call(null);
+calling.apply(null);
+calling.bind(null);
+
+function* Func() {
+    this.fn1 = function() {};
+    this.fn2 = () => {};
+    this.fn3 = arg => {};
+    this.var = 0;
+    yield;
+    return;
 }
+
+Func.prototype.fn4 = () => {};
+
+Func.call();
+
+String();
+Boolean();
+Array.isArray();
+Number.isNaN();
+Number.prototype.parseInt.call();
+
+var obj = {
+    get prop () {},
+    set prop (v) {},
+    shortFn() {},
+    fn: () => {},
+    key1: 0,
+    [key2]: 0,
+};
+
+class Duck extends Animal {
+    constructor() {}
+    quack() { super.quack(); }
+    static count1() {}
+}
+
+var FemaleDuck = class extends Duck {};
+
+var d = new Duck();
+
+// constructs
+
+if (a === true) {}
+else {}
+
+switch (val) {
+    default:
+    case 0:
+        break;
+}
+
+for (let i = 0; i < 10; i++) {
+    continue;
+}
+
+while (a !== 1 && b == true) {}
+
+do {} while (c <= 0 || d >= 1);
+
+try {}
+catch (err) {}
+finally {}
+
+// global
+
+global.setTimeout();
+window.setInterval();
+console.log();
+
+JSON.parse();
+
+// node
+
+var mod = require('mod');
+
+exports.fn = () => {};
+module.exports = { n, x };
+
+process.exit(-1);
+process.env.PORT;
